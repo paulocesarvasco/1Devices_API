bf1d1ea671f85e1653262ec057b3e2b13463b601
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-05 11:01:02 -0300

    Define controllers

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,8 +1,24 @@
 package handler
 
+import "net/http"
+
 type Handler interface {
-	DeleteDevice()
-	RegisterDevice()
-	SearchDevice()
-	UpdateDevice()
+	DeleteDevice(w http.ResponseWriter, r *http.Request)
+	RegisterDevice(w http.ResponseWriter, r *http.Request)
+	SearchDevice(w http.ResponseWriter, r *http.Request)
+	UpdateDevice(w http.ResponseWriter, r *http.Request)
 }
+
+func NewHandler() Handler {
+	return &handler{}
+}
+
+type handler struct{}
+
+func (h *handler) DeleteDevice(w http.ResponseWriter, r *http.Request) {}
+
+func (h *handler) RegisterDevice(w http.ResponseWriter, r *http.Request) {}
+
+func (h *handler) SearchDevice(w http.ResponseWriter, r *http.Request) {}
+
+func (h *handler) UpdateDevice(w http.ResponseWriter, r *http.Request) {}
