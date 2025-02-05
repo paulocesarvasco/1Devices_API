ea3c5cdd7101e9c89e560d3ea0acaa0303190189
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-05 14:53:56 -0300

    Implement search tests

--- internal/handler/handlers_test.go
+++ internal/handler/handlers_test.go
@@ -4,6 +4,7 @@ import (
 	"1Devices_API/internal/resources"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,5 +29,55 @@ func TestInsertDevice(t *testing.T) {
 		rr := httptest.NewRecorder()
 		h.RegisterDevice(rr, req)
 		assert.Equal(t, tc.expectedCode, rr.Code)
+	}
+}
+
+func TestSearchSingleDevice(t *testing.T) {
+	tt := []struct {
+		name            string
+		queryParameter  string
+		queryValue      string
+		expectedCode    int
+		expectedPayload any
+	}{
+		{"Search ID", "id", "123", http.StatusOK, resources.Device{}},
+		{"Device not found", "id", "124", http.StatusNotFound, resources.Device{}},
+	}
+	for _, tc := range tt {
+		h := NewHandler()
+		url := fmt.Sprintf("http://localhost:8080/api/v1/devices?%s=%s", tc.queryParameter, tc.queryValue)
+		req, _ := http.NewRequest(http.MethodGet, url, nil)
+		rr := httptest.NewRecorder()
+		h.SearchDevice(rr, req)
+		assert.Equal(t, tc.expectedCode, rr.Code)
+		var response resources.Device
+		json.Unmarshal(rr.Body.Bytes(), &response)
+		assert.Equal(t, tc.expectedPayload, response)
+	}
+}
+
+func TestSearchMultDevices(t *testing.T) {
+	tt := []struct {
+		name            string
+		queryParameter  string
+		queryValue      string
+		expectedCode    int
+		expectedPayload any
+	}{
+		{"Retrieve all devices", "", "", http.StatusOK, []resources.Device{}},
+		{"Fetch by brand", "brand", "xPhone", http.StatusOK, []resources.Device{}},
+		{"Fetch by state", "state", "available", http.StatusOK, []resources.Device{}},
+		{"Device not found", "brand", "Android", http.StatusNotFound, nil},
+	}
+	for _, tc := range tt {
+		h := NewHandler()
+		url := fmt.Sprintf("http://localhost:8080/api/v1/devices?%s=%s", tc.queryParameter, tc.queryValue)
+		req, _ := http.NewRequest(http.MethodGet, url, nil)
+		rr := httptest.NewRecorder()
+		h.SearchDevice(rr, req)
+		assert.Equal(t, tc.expectedCode, rr.Code)
+		var response resources.Device
+		json.Unmarshal(rr.Body.Bytes(), &response)
+		assert.Equal(t, tc.expectedPayload, response)
 	}
 }
