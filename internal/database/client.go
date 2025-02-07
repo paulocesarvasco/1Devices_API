38f578ec92b8aded780a5e533f0242db5df2e850
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-07 16:53:46 -0300

    Use postgreSQL as permanent database

--- internal/database/client.go
+++ internal/database/client.go
@@ -4,8 +4,11 @@ import (
 	"1Devices_API/internal/constants"
 	"1Devices_API/internal/resources"
 	"errors"
+	"fmt"
 	"log"
+	"os"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -18,10 +21,35 @@ type Client interface {
 	FetchDevicesByBrand(brand string) ([]resources.Device, error)
 	RemoveDevice(id int) error
 	UpdateDevice(currentValues resources.Device, newValues resources.Device) error
+	RunMigrations()
 }
 
-type sqliteClient struct {
+type dbClient struct {
 	db *gorm.DB
+}
+
+func (c *dbClient) RunMigrations() {
+	err := c.db.AutoMigrate(&resources.Device{})
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+	log.Println("Migration completed successfully!")
+}
+
+func NewPostgresClient() Client {
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	return &dbClient{db: db}
 }
 
 func NewSQLiteClient() Client {
@@ -32,10 +60,10 @@ func NewSQLiteClient() Client {
 
 	db.AutoMigrate(&resources.Device{})
 
-	return &sqliteClient{db: db}
+	return &dbClient{db: db}
 }
 
-func (c *sqliteClient) InsertDevice(device resources.Device) (resources.Device, error) {
+func (c *dbClient) InsertDevice(device resources.Device) (resources.Device, error) {
 	tx := c.db.Begin()
 	defer func() {
 		if r := recover(); r != nil {
@@ -57,7 +85,7 @@ func (c *sqliteClient) InsertDevice(device resources.Device) (resources.Device, 
 	return device, nil
 }
 
-func (c *sqliteClient) SelectDevice(id int) (resources.Device, error) {
+func (c *dbClient) SelectDevice(id int) (resources.Device, error) {
 	var device resources.Device
 	result := c.db.First(&device, id)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
@@ -69,7 +97,7 @@ func (c *sqliteClient) SelectDevice(id int) (resources.Device, error) {
 	return device, nil
 }
 
-func (c *sqliteClient) FetchAllDevices() ([]resources.Device, error) {
+func (c *dbClient) FetchAllDevices() ([]resources.Device, error) {
 	var devices []resources.Device
 	result := c.db.Find(&devices)
 	if result.Error != nil {
@@ -78,7 +106,7 @@ func (c *sqliteClient) FetchAllDevices() ([]resources.Device, error) {
 	return devices, nil
 }
 
-func (c *sqliteClient) FetchDevicesByBrand(brand string) ([]resources.Device, error) {
+func (c *dbClient) FetchDevicesByBrand(brand string) ([]resources.Device, error) {
 	var devices []resources.Device
 	result := c.db.Where("brand = ?", brand).Find(&devices)
 	if result.Error != nil {
@@ -87,7 +115,7 @@ func (c *sqliteClient) FetchDevicesByBrand(brand string) ([]resources.Device, er
 	return devices, nil
 }
 
-func (c *sqliteClient) FetchDevicesByState(state string) ([]resources.Device, error) {
+func (c *dbClient) FetchDevicesByState(state string) ([]resources.Device, error) {
 	var devices []resources.Device
 	result := c.db.Where("state = ?", state).Find(&devices)
 	if result.Error != nil {
@@ -96,7 +124,7 @@ func (c *sqliteClient) FetchDevicesByState(state string) ([]resources.Device, er
 	return devices, nil
 }
 
-func (c *sqliteClient) RemoveDevice(id int) error {
+func (c *dbClient) RemoveDevice(id int) error {
 	var device resources.Device
 	result := c.db.Where("id = ?", id).First(&device)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
@@ -112,7 +140,7 @@ func (c *sqliteClient) RemoveDevice(id int) error {
 	return nil
 }
 
-func (c *sqliteClient) UpdateDevice(currentValues resources.Device, newValues resources.Device) error {
+func (c *dbClient) UpdateDevice(currentValues resources.Device, newValues resources.Device) error {
 	if err := c.db.Model(&currentValues).Updates(newValues).Error; err != nil {
 		return err
 	}
